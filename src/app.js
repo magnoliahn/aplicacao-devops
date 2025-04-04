e0dde95aadefbff0839fc110d7770c771de52149
sofiacgodinho <sofia.godinho1@icloud.com>
2025-04-04 20:04:39 -0300

    criei o index.html e o endpoint

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`App rodando em http://localhost:${port}`);
+});
+const data = {
+    integrantes: [
+      { nome: 'Magnólia Hiller' },
+      { nome: 'Aline Teixeira' }
+    ]
+  };
+  
+  app.get('/integrantes', (req, res) => {
+    res.json(data);
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile('views/index.html', { root: __dirname });
+  });
+  
